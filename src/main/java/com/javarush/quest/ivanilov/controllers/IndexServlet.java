e0467a759dff02d8b19ef289b9b3999b07853ee6
aivanilov <ivanilov@inbox.ru>
2023-02-06 14:41:35 +0700

    Initialization in Listener.

--- src/main/java/com/javarush/quest/ivanilov/controllers/IndexServlet.java
+++ src/main/java/com/javarush/quest/ivanilov/controllers/IndexServlet.java
@@ -1,12 +1,9 @@
 package com.javarush.quest.ivanilov.controllers;
 
-import com.javarush.quest.ivanilov.config.Config;
-import com.javarush.quest.ivanilov.services.*;
 import com.javarush.quest.ivanilov.utils.Navigator;
 import com.javarush.quest.ivanilov.utils.constants.Attributes;
 import com.javarush.quest.ivanilov.utils.constants.Jsp;
 import com.javarush.quest.ivanilov.utils.constants.Targets;
-import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -18,18 +15,6 @@ import java.io.IOException;
 @Log4j2
 @WebServlet(name = "IndexServlet", value = Targets.INDEX)
 public class IndexServlet extends HttpServlet {
-
-    @Override
-    public void init(ServletConfig config) {
-        QuestParser parser = new QuestParser(QuestService.QUEST_SERVICE,
-                EventService.EVENT_SERVICE,
-                TaskService.TASK_SERVICE,
-                HeroService.HERO_SERVICE);
-
-        Config cfg = new Config(parser, UserService.USER_SERVICE);
-        cfg.readUsersAndQuestsFromResources();
-        log.info("The app has been initialized.");
-    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
