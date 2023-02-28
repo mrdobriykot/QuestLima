4ce46bfd1cbd530361a2f1b0c8ca9d4a7f2b4fd4
chuu100 <106085848+chuu100@users.noreply.github.com>
2023-02-28 22:53:50 +0900

    Update Game.java

--- src/main/java/com/javarush/mokropolov/entity/Game.java
+++ src/main/java/com/javarush/mokropolov/entity/Game.java
@@ -1,1 +1,25 @@
-package com.javarush.mokropolov.entity;import lombok.AllArgsConstructor;import lombok.Builder;import lombok.Data;import lombok.NoArgsConstructor;@Data@NoArgsConstructor@AllArgsConstructor@Builderpublic class Game implements Entity{    private Long id;    private Long questId;    private Long userId;    private Long currentQuestionId;    private GameState gameState;}
+package com.javarush.mokropolov.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Game implements Entity{ 
+
+    private Long id;
+
+    private Long questId;
+
+    private Long userId;
+
+    private Long currentQuestionId;
+
+    private GameState gameState;
+
+
+}
