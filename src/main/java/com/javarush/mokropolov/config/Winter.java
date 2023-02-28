9371d9c13a9bff2eded43fb8bebc1e03babe831a
chuu100 <106085848+chuu100@users.noreply.github.com>
2023-02-28 22:50:28 +0900

    Update Winter.java

--- src/main/java/com/javarush/mokropolov/config/Winter.java
+++ src/main/java/com/javarush/mokropolov/config/Winter.java
@@ -1,1 +1,36 @@
-package com.javarush.mokropolov.config;import lombok.experimental.UtilityClass;import java.lang.reflect.Constructor;import java.lang.reflect.InvocationTargetException;import java.util.HashMap;import java.util.Map;@UtilityClasspublic class Winter {    private final Map<Class<?>, Object> container = new HashMap<>();    @SuppressWarnings("unchecked")    public <T> T getBean(Class<T> type) {        try {            if (container.containsKey(type)) {                return (T) container.get(type);            } else {                Constructor<?>[] constructors = type.getConstructors();                Constructor<?> constructor = constructors[0];                Class<?>[] parameterTypes = constructor.getParameterTypes();                Object[] parameters = new Object[parameterTypes.length];                for (int i = 0; i < parameters.length; i++) {                    parameters[i] = getBean(parameterTypes[i]);                }                Object component = constructor.newInstance(parameters);                container.put(type, component);                return (T) component;            }        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {            throw new RuntimeException("Context broken for " + type, e);        }    }}
+package com.javarush.mokropolov.config;
+
+import lombok.experimental.UtilityClass;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
+@UtilityClass
+public class Winter {
+  
+    private final Map<Class<?>, Object> container = new HashMap<>();
+
+    @SuppressWarnings("unchecked")
+    public <T> T getBean(Class<T> type) {
+        try {
+            if (container.containsKey(type)) {
+                return (T) container.get(type);
+            } else {
+                Constructor<?>[] constructors = type.getConstructors();
+                Constructor<?> constructor = constructors[0];
+                Class<?>[] parameterTypes = constructor.getParameterTypes();
+                Object[] parameters = new Object[parameterTypes.length];
+                for (int i = 0; i < parameters.length; i++) {
+                    parameters[i] = getBean(parameterTypes[i]);
+                }
+                Object component = constructor.newInstance(parameters);
+                container.put(type, component);
+                return (T) component;
+            }
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException("Context broken for " + type, e);
+        }
+    }
+}
